2333ba9c32299330500076a11bcefa6f96bfa38b
minhnd-dev <nguyenducminh.dev@gmail.com>
2024-03-16 10:12:45 +0700

    Fix login API

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
 import bcrypt
-from flask import Flask, request, url_for, jsonify, make_response, redirect
-import flask
-import pyodbc
+from flask import Flask, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
-con_str = "DRIVER={SQL Server};SERVER=LAPTOP-IP4EHMEC\\SQLEXPRESS;DATABASE=easy_ace;Trusted_Connection=yes"
-conn = pyodbc.connect(con_str)
-app.config['SECRET_KEY'] = 'thisisthesecretkey'
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+conn = sqlite3.connect("test.db", check_same_thread=False)
 
 
 @app.route('/user/getbyid/<id>', methods=['GET'])
 def get_by_id(id):
     try:
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM Users WHERE id=?", id)
+        cursor.execute("SELECT * FROM users WHERE id=?", id)
         results = []
         keys = [i[0] for i in cursor.description]
         for val in cursor.fetchall():
             results.append(dict(zip(keys, val)))
-        resp = flask.jsonify(results)
+        resp = jsonify(results)
         resp.status_code = 200
         return resp
     except Exception as e:
@@ -33,15 +25,15 @@ def get_by_id(id):
 @app.route('/user/add', methods=['POST'])
 def add_user():
     try:
-        id = flask.request.json.get("id")
-        username = flask.request.json.get("username")
-        password = flask.request.json.get("password")
-        sql = 'INSERT INTO Users(id, username, password) VALUES (?, ?, ?)'
+        id = request.json.get("id")
+        user_name = request.json.get("user_name")
+        password = request.json.get("password")
+        sql = 'INSERT INTO users(id, user_name, password) VALUES (?, ?, ?)'
         cursor = conn.cursor()
-        data = (id, username, password)
+        data = (id, user_name, password)
         cursor.execute(sql, data)
         conn.commit()
-        resp = flask.jsonify('Add successfully')
+        resp = jsonify('Add successfully')
         resp.status_code = 200
         return resp
     except Exception as e:
@@ -51,14 +43,14 @@ def add_user():
 @app.route('/user/edit', methods=['PUT'])
 def update_user():
     try:
-        id = flask.request.json.get("id")
-        username = flask.request.json.get("username")
+        id = request.json.get("id")
+        user_name = request.json.get("user_name")
         cursor = conn.cursor()
-        sql = "update Users set username = ? where id = ?"
-        data = (username, id)
+        sql = "update users set user_name = ? where id = ?"
+        data = (user_name, id)
         cursor.execute(sql, data)
         conn.commit()
-        resp = flask.jsonify({"mess": "success"})
+        resp = jsonify({"mess": "success"})
         resp.status_code = 200
         return resp
     except Exception as e:
@@ -68,45 +60,47 @@ def update_user():
 @app.route('/user/delete', methods=['DELETE'])
 def delete_user():
     try:
-        id = flask.request.json.get("id")
+        id = request.json.get("id")
         cursor = conn.cursor()
-        sql = "delete Users where id = ?"
+        sql = "delete users where id = ?"
         data = (id)
         cursor.execute(sql, id)
         conn.commit()
-        resp = flask.jsonify({"mess": "success"})
+        resp = jsonify({"mess": "success"})
         resp.status_code = 200
         return resp
     except Exception as e:
         print(e)
 
-@app.route('/register', methods = ['POST'])
+
+@app.route('/register', methods=['POST'])
 def register():
     id = request.json.get("id", None)
-    username = request.json.get("username", None)
+    user_name = request.json.get("user_name", None)
     password = request.json.get("password", None)
-    if not username:
-        return "Missing username", 400
+    if not user_name:
+        return "Missing user_name", 400
     if not password:
         return "Missing password", 400
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    sql = 'INSERT INTO Users(id, username, password) VALUES (?, ?, ?)'
+    sql = 'INSERT INTO users(id, user_name, password) VALUES (?, ?, ?)'
     cursor = conn.cursor()
-    data = (id, username, hashed)
+    data = (id, user_name, hashed)
     cursor.execute(sql, data)
     conn.commit()
-    return f"Welcome {username}"
+    return f"Welcome {user_name}"
 
-@app.route('/login', methods = ['POST'])
+
+@app.route('/login', methods=['POST'])
 def login():
-    username = request.json.get("username", None)
+    user_name = request.json.get("user_name", None)
     password = request.json.get("password", None)
-    if not username:
-        return "Missing username", 400
+    if not user_name:
+        return "Missing user_name", 400
     if not password:
         return "Missing password", 400
     cursor = conn.cursor()
-    user = cursor.execute("SELECT * FROM Users WHERE username=?", username)
+    user = cursor.execute("SELECT * FROM users WHERE user_name=?", (user_name, ))
 
     results = []
     keys = [i[0] for i in cursor.description]
@@ -115,10 +109,11 @@ def login():
 
     if not user:
         return "User not found", 404
-    if bcrypt.checkpw(password.encode('utf-8'), results[0]["password"].encode('utf-8')) == results[0]["password"].encode('utf-8'):
-        return f"Welcome back {username}"
+    if bcrypt.checkpw(password.encode('utf-8'), results[0]["password"]):
+        return f"Welcome back {user_name}"
     else:
         return "Wrong password!"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
