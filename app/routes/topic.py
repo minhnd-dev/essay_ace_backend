e83867a1f86785929609bbe3f0185f1b5577b77e
minhnd-dev <nguyenducminh.dev@gmail.com>
2024-04-16 23:27:19 +0700

    Create get ai model service

--- app/routes/topic.py
+++ app/routes/topic.py
@@ -5,6 +5,7 @@ from flask import Blueprint
 from app.database import Session
 from app.models.topic import Topic
 from app.schema.topic import TopicSchema, DeleteSchema, CreateTopicSchema
+from app.services.ai_model import AIModel
 from app.services.jwt_service import jwt_required
 from app.services.validation_service import validate_body
 
@@ -35,7 +36,7 @@ def get(body: TopicSchema, current_user):
     }
 
 
-@topic_bp.route('/delete', methods=["DELETE"])
+@topic_bp.route("/", methods=["DELETE"])
 @validate_body(DeleteSchema)
 def delete(body: DeleteSchema):
     session = Session()
@@ -46,19 +47,19 @@ def delete(body: DeleteSchema):
 
         return {"message": "Topic deleted successfully"}, 200
     else:
-        return {"message": "Topic not found or you don't have permission to delete it"}, 404
+        return {
+            "message": "Topic not found or you don't have permission to delete it"
+        }, 404
 
 
-@topic_bp.route('/create', methods=["POST"])
+@topic_bp.route("/", methods=["POST"])
 @jwt_required
 @validate_body(CreateTopicSchema)
 def create(body: CreateTopicSchema, current_user):
     session = Session()
     try:
         new_topic = Topic(
-            user_id=current_user.id,
-            content=body.content,
-            created_at=datetime.now()
+            user_id=current_user.id, content=body.content, created_at=datetime.now()
         )
         session.add(new_topic)
         session.commit()
@@ -70,3 +71,11 @@ def create(body: CreateTopicSchema, current_user):
         session.close()
 
 
+@topic_bp.route("/ai", methods=["GET"])
+# @jwt_required
+def get_ai_generated_topic():
+    ai = AIModel()
+    return {
+        "message": "Successfully generated topic",
+        "data": ai.generate_topic(),
+    }
